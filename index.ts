ce5f66a291437a009cf2600d2d39c711d922ddd0
donggoolosori <kdj6551@naver.com>
2021-11-27 18:09:00 +0900

    feat: typeorm으로 db 연결

--- index.ts
+++ index.ts
@@ -1,10 +1,32 @@
 import express from 'express';
 import morgan from 'morgan';
+import { createConnection } from 'typeorm';
 import userRouter from './api';
+import { User } from './model/user';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 
 app.use(morgan('dev'));
+app.use(express.json());
+
+createConnection({
+  type: 'mysql',
+  host: 'localhost',
+  port: +process.env.PORT!,
+  username: process.env.USER_NAME,
+  password: process.env.PASS_WORD,
+  database: process.env.DATABASE,
+  entities: [User],
+  synchronize: true,
+  logging: false,
+})
+  .then(() => {
+    console.log('db is connected.');
+  })
+  .catch((error) => console.log(error));
 
 app.use('/api/user', userRouter);
 
