bcbd1941a774d0bed44bee8b2736989d44148ea4
donggoolosori <kdj6551@naver.com>
2021-11-27 21:03:39 +0900

    feat: intersection observer를 사용해 무한 스크롤 구현

--- client/app.js
+++ client/app.js
@@ -1,13 +1,52 @@
 const userInfoBox = document.querySelector('.user-info-box');
-
-const url = 'http://localhost:3000/api/user';
+const footer = document.querySelector('footer');
 
 const fetchData = (url) => {
   const response = fetch(url);
   return response.then((res) => res.json());
 };
 
-window.onload(() => {
-  const data = await fetchData(url);
-  console.log(data);
-});
+const drawItems = async (url, offset) => {
+  const data = await fetchData(`${url}/${offset}`);
+
+  const element = data.reduce((prev, { name, email, gender }) => {
+    return (
+      prev +
+      `
+      <div class="user-info">
+        <h1>${name}</h1>
+        <div class="email-gender">
+          ${email} (${gender})
+        </div>
+      </div>
+    `
+    );
+  }, '');
+
+  userInfoBox.insertAdjacentHTML('beforeend', element);
+};
+
+const getloadItems = () => {
+  const url = 'http://localhost:3000/api/user';
+  let offset = 0;
+
+  return () => {
+    drawItems(url, offset);
+    offset += 20;
+  };
+};
+
+const loadItems = getloadItems();
+
+const io = new IntersectionObserver(
+  (entry) => {
+    if (entry[0].isIntersecting) {
+      loadItems();
+    }
+  },
+  { threshold: 0.5 }
+);
+
+loadItems();
+
+io.observe(footer);
